b51d311dcfc9a53c41ece93f8f13b7b90185fdb3
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-04-20 14:10:50 -0500

    Generate playing escenario

--- main.js
+++ main.js
@@ -35,9 +35,9 @@ class Effect extends Card {
 			} else if (this.magnitude >= 0 && this.stat == "resiliencia") {
 				target.res += this.magnitude
 			} else if (this.magnitude < 0 && this.stat == "poder") {
-				target.power -= this.magnitude
+				target.power += this.magnitude
 			} else {
-				target.res -= this.magnitude
+				target.res += this.magnitude
 			}
 		}
     	else {
@@ -46,15 +46,15 @@ class Effect extends Card {
 	}
 }
 
-/* Iniciar objetos */
+/* Iniciar efectos */
 
-const blackBeltNinja = new Unit("Ninja Cinturon Negro", 5, 4, 5)
 const algDificil = new Effect("Algoritmo difícil",2,"Aumenta la resistencia del objetivo en 3","resiliencia", +3)
 const rechazoProm = new Effect("Rechazo de promesa no manejado", 1, "Reduce la resistencia del objetivo en 2", "resiliencia", -2)
-const pairProg = new Effect("Programación en pareja", 3, "Aumenta el poder del objetivo en 2", +2)
+const pairProg = new Effect("Programación en pareja", 3, "Aumenta el poder del objetivo en 2","poder", +2)
 
 /* Escenario */
-/* Primera jugada */
+console.log("---------------------------------------")
+console.log("Primera jugada")
 const redBeltNinja = new Unit("Ninja Cinturon Rojo", 3, 3, 4)
 console.log(`El jugador uno invoca la carta ${redBeltNinja.name}`)
 console.log(redBeltNinja)
@@ -62,3 +62,21 @@ algDificil.play(redBeltNinja)
 console.log(`El jugador uno usa ${algDificil.name} en ${redBeltNinja.name}`)
 console.log(algDificil.definition)
 console.log(redBeltNinja)
+console.log("---------------------------------------")
+console.log("Segunda jugada")
+const blackBeltNinja = new Unit("Ninja Cinturon Negro", 4, 5, 4)
+console.log(`El jugador dos invoca la carta ${blackBeltNinja.name}`)
+console.log(blackBeltNinja)
+console.log(`El jugador dos usa ${rechazoProm.name} contra ${redBeltNinja.name}`)
+console.log(rechazoProm.definition)
+rechazoProm.play(redBeltNinja)
+console.log(redBeltNinja)
+console.log("---------------------------------------")
+console.log("Tercera jugada")
+console.log(`El jugador uno usa ${pairProg.name} contra ${blackBeltNinja.name}`)
+console.log(pairProg.definition)
+pairProg.play(blackBeltNinja)
+console.log(blackBeltNinja)
+console.log(`El jugador uno ataca ${blackBeltNinja.name} con ${redBeltNinja.name}`)
+redBeltNinja.attack(blackBeltNinja)
+console.log(blackBeltNinja)
